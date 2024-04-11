5fa08bcd29671ddb42547f9957e0606baffee6d8
ReaperSans28 <139446274+ReaperSans28@users.noreply.github.com>
2024-04-11 12:16:44 +0300

    Changed mask_account_number

--- src/masks.py
+++ src/masks.py
@@ -14,4 +14,4 @@ def mask_account_number(user_account_number: str) -> str:
     Функция принимает номер счета и возвращает маску счета.
     """
     masked_number = "**" + user_account_number[-4:]
-    return masked_number
+    return f"Счет {masked_number}"
