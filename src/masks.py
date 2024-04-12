7a27342bac98f7fdba047a08898060be98456ced
ReaperSans28 <139446274+ReaperSans28@users.noreply.github.com>
2024-04-12 11:40:23 +0300

    Update masks.py
    back to 9.1

--- src/masks.py
+++ src/masks.py
@@ -2,11 +2,8 @@ def mask_card_number(user_card_number: str) -> str:
     """
     Функция принимает номер банковской карты и возвращает ее маску.
     """
-    user_card = user_card_number.split()
-    number = user_card[-1]
-    card_name = user_card[:-1]
-    masked_number = " " + number[:4] + " " + number[4:6] + "** **** " + number[12:]
-    return " ".join(card_name) + masked_number
+    masked_number = user_card_number[:4] + " " + user_card_number[4:6] + "** **** " + user_card_number[12:]
+    return masked_number
 
 
 def mask_account_number(user_account_number: str) -> str:
@@ -14,4 +11,5 @@ def mask_account_number(user_account_number: str) -> str:
     Функция принимает номер счета и возвращает маску счета.
     """
     masked_number = "**" + user_account_number[-4:]
-    return f"Счет {masked_number}"
+    return masked_number
+    
