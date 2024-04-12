a34ec42f2046040a9b8d221458c4e18adbbf8857
ReaperSans28 <139446274+ReaperSans28@users.noreply.github.com>
2024-04-12 11:41:13 +0300

    Update widget.py
    back to 9.2

--- src/widget.py
+++ src/widget.py
@@ -14,6 +14,9 @@ def number_or_account(user_input: str) -> str:
     Функция определяет работаем мы с счетом или картой.
     """
     if "Счет" in user_input:
-        return mask_account_number(user_input)
+        return f"Счет {mask_account_number(user_input)}"
     else:
-        return mask_card_number(user_input)
+        user_card = user_input.split()
+        card_name = user_card[:-1]
+        card_number = user_card[-1]
+        return " ".join(card_name) + " " + mask_card_number(card_number)
